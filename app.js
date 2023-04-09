f9cfdd549a05f48395e8a561b513e7a7fa05c334
Wei <awei1127@hotmail.com>
2023-04-09 20:48:07 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -32,6 +33,20 @@ app.get('/', (req, res) => {
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name             // 從 req.body 拿出表單裡的 name 資料
+  const todo = new Todo({
+    name: name
+  })
+  return todo.save()                 // 存入資料庫
+    .then(() => { res.redirect('/') }) // 新增完成後導回首頁
+    .catch(error => { console.error(error) })
 })
 
 app.listen(port, () => {
