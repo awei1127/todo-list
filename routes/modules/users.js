c4145c00eb1d1890d02304f0b6d3adbffe511578
Wei <awei1127@hotmail.com>
2023-04-26 04:18:03 +0800

    feat: add registeration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // 定義路由
 // 登入頁面
@@ -8,8 +9,24 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// 註冊頁面
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+// 送出註冊資訊
+router.post('/register', (req, res) => {
+  const { username, email, password, confirmPassword } = req.body
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      console.log('already exists.')
+      res.render('register', { username, email, password, confirmPassword })
+    } else {
+      return User.create({ username, email, password })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    }
+  })
 })
 
 module.exports = router
