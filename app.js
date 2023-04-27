f2084322491fa2c5695da1ed9b901081b3ef0f39
Wei <awei1127@hotmail.com>
2023-04-27 17:12:17 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const session = require('express-session')
 
 //Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
 require('./config/mongoose')
@@ -13,6 +14,11 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // 執行並監聽
 app.listen(port, () => {
