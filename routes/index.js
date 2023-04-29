dcc20a2cd02546874c7d63be730b6e05de95855b
Wei <awei1127@hotmail.com>
2023-04-30 01:47:49 +0800

    modify: fix comment

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,13 @@ const todos = require('./modules/todos')
 // 引入路由模組：引入 users 模組程式碼
 const users = require('./modules/users')
 
-// 引入驗證用中間件
+// 引入驗證用中介軟體
 const { authenticator } = require('../middleware/auth')
 
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', authenticator, todos)
 
-// 將網址結構符合 /users 字串開頭的 request 導向 todos 模組 
+// 將網址結構符合 /users 字串開頭的 request 導向 users 模組 
 router.use('/users', users)
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
